262007f7cb31fe83aa4fe05288c9be6bf190e801
joshimalay <malayjoshi45@gmail.com>
2025-01-01 19:51:46 +0530

    Intial Commit

---
+++ Chapter 10/01_class.py
@@ -0,0 +1,16 @@
+class Employee:
+    # Class attributes
+    language = "Python"
+    salary = 10000
+    
+
+# Create an object of the class
+harry = Employee()
+# Object attribute
+harry.name = "Harry code with wala"
+print(harry.language, harry.salary, harry.name)
+
+malay = Employee()
+# Object attribute
+malay.name = "Malay joshi"
+print(malay.salary, malay.language, malay.name)
